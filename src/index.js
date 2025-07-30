de181a666df36c0b19c8628cdbfd32f5744d9ab3
eyeej <eyee.joy@gmail.com>
2025-07-30 15:28:45 +1000

    api included

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-function aiPoem(event) {
-    event.preventDefault();
-
+function displayPoem(response) {
     new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
         });
+    
+}
+
+function aiPoem(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "t9e9139a19b801fbcfa020d17a47ob1f";
+    let prompt = `Generate a French poem about ${instructionsInput.value}`;
+    let context = "You need to generate a roman poem in French. Keep the poem short and sweet, 4 lines maxmimum with a <br/> between each line. . Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiURL).then(displayPoem);
+    
  }
 
 let poemFormElement = document.querySelector("#poem-generator");
